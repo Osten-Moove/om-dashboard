9020ac9503fe1b29801498e505966d66a3225a95
ferreira-guilherme-s <souza.guilhermegferreira@gmail.com>
2024-11-08 11:26:48 -0300

    update -> atualizando métodos do serviço de variáveis

--- src/services/VariablesService.ts
+++ src/services/VariablesService.ts
@@ -1,17 +1,21 @@
-import { filterSeparate, RejectedInputDTO, ServiceDTO } from '@duaneoli/base-project-nest';
+import { ExceptionDTO, filterSeparate, RejectedInputDTO, ServiceDTO } from '@duaneoli/base-project-nest';
 import { Injectable } from '@nestjs/common';
 import { all, create } from 'mathjs';
 import { VariablesEntity } from '../entities/VariablesEntity';
 import { ErrorCodeEnum } from '../enums/ErrorCodeEnum';
 import { replaceFunctionCall } from '../helpers/query';
-import { IVariablesInterface } from '../interfaces/IVariablesInterface';
 import { DashboardQueriesModule } from '../module/DashboardQueriesModule';
-import { CreateVariable, GetCountQueriesVariablesType, ValidateVariable } from '../types/VariablesTypes';
+import { CreateVariable, OperationType, UpdateVariablesType, VariableType } from '../types/VariablesTypes';
 import { In, Repository } from 'typeorm';
-import { transformObject } from '../helpers/TransformObject';
+import crypto from 'crypto';
+
+function createUniqueHash(params: Array<string>) {
+  const hashParams = params.join(',');
+  return crypto.createHash('sha256').update(hashParams).digest('hex');
+}
 
 @Injectable()
-export class VariablesService implements IVariablesInterface {
+export class VariablesService {
   private repository: Repository<VariablesEntity>;
 
   constructor() {
@@ -26,80 +30,87 @@ export class VariablesService implements IVariablesInterface {
     }
   }
 
+  async update(data: Array<UpdateVariablesType>) {
+    const variablesInDatabase = await this.repository.find({ where: { id: In(data.map((it) => it.id)) } });
+    const [process, reject] = filterSeparate(data, () => variablesInDatabase.some((it) => it.id === it.id));
+
+    const saveEntities = await this.repository.save(process);
+    return new ServiceDTO(saveEntities);
+  }
+
   async execute(query: string, params?: Array<string>): Promise<number> {
     const sql = replaceFunctionCall(query, [...(params || [])]);
-    const constResult = await this.repository.query(sql);
+    const countResult = await this.repository.query(sql);
 
-    if (!constResult || constResult.length === 0 || !constResult[0].value) return 0;
+    if (!countResult || countResult.length === 0 || !countResult[0].value)
+      throw ExceptionDTO.warn('Não foi possível obter a contagem.', 'Verify if sql return column value');
 
-    return Number(constResult[0].value);
+    return Number(countResult[0].value);
   }
 
-  async getVariableCount(id: string, params?: Array<string>): Promise<any> {
+  async getVariableCalc(id: string, params?: Array<string>) {
     const entity = await this.repository.findOne({ where: { id } });
-    const countResult = await this.execute(entity.query, params);
-    return { ...entity, value: countResult };
+
+    try {
+      const countResult = await this.execute(entity.query, params);
+      return { ...entity, value: countResult };
+    } catch (error) {
+      throw ExceptionDTO.warn(error.message, 'Query failed, query executed: ' + error.query);
+    }
   }
 
-  async getQueriesVariableCount(variables: Array<GetCountQueriesVariablesType>): Promise<ServiceDTO<any>> {
+  async operationCalcVariables(operations: Array<OperationType>) {
     const rejectedInputs = new Array<RejectedInputDTO>();
-    const entities = await this.repository.find({
-      where: { id: In(variables.map((v) => v.id)) },
-    });
-    const [processable, rejected] = filterSeparate(variables, (variable) =>
-      entities.some((entity) => String(entity.id) === String(variable.id)),
-    );
-    rejected.forEach((variable) => {
-      rejectedInputs.push(new RejectedInputDTO(String(variable.id), 'Variable not found in database', 400));
+    const variablesId = operations.flatMap((it) => Object.values(it.variables).map((itt) => itt.id));
+    const entities = await this.repository.find({ where: { id: In(variablesId) } });
+
+    variablesId
+      .filter((variable) => !entities.some((entity) => String(entity.id) === String(variable)))
+      .forEach((variable) => {
+        rejectedInputs.push(new RejectedInputDTO(String(variable), 'Variable not found in database', 0));
+      });
+    if (rejectedInputs.length > 0)
+      throw ExceptionDTO.warn('Variables not found', 'Some variables not found in database', rejectedInputs);
+
+    const newOperations = new Array<
+      Omit<OperationType, 'variables'> & { variables: Record<string, string>; result?: any }
+    >();
+    const variablesToPromise: Record<string, VariableType> = {};
+
+    operations.forEach((operation) => {
+      const newOperation = { ...operation, variables: {} };
+      Object.entries(operation.variables).forEach(([key, value]) => {
+        const hash = value.id + createUniqueHash(value.params);
+        newOperation.variables[key] = hash;
+        variablesToPromise[hash] = value;
+      });
+      newOperations.push(newOperation);
     });
 
-    const variablePromises = processable.map(async (variable) => {
+    const variablePromises = Object.entries(variablesToPromise).map(async ([hash, variable]) => {
       const entity = entities.find((e) => String(e.id) === String(variable.id));
       const params = [...(Array.isArray(variable.params) ? variable.params : [])];
       const result = await this.execute(entity.query, params);
       return {
-        searchId: variable.searchId,
-        type: entity.unitOfMeasure,
+        hash,
         value: result,
       };
     });
 
     const results = await Promise.all(variablePromises);
-    return new ServiceDTO(results, results.length, rejectedInputs);
-  }
+    const hashToValue = results.reduce((acc, result) => {
+      acc[result.hash] = result.value;
+      return acc;
+    }, {});
 
-  async getCalc(calculations: Array<ValidateVariable>): Promise<any> {
     const math = create(all);
-
-    const calculationPromises = calculations.map(async (calc) => {
-      const operation = math.compile(calc.operation);
-      const queries = Object.values(calc.queries);
-
-      const parsedParams: Array<GetCountQueriesVariablesType> = queries.map((value) => ({
-        id: value.id as any,
-        searchId: value.operator as any,
-        params: value.params as any,
-      }));
-
-      const resultQueries = await this.getQueriesVariableCount(parsedParams);
-      const result = resultQueries.entities;
-
-      const scope = queries.reduce((acc, query) => {
-        const counts = result.find((item) => item.searchId === query.operator).value || 0;
-        acc[query.operator] = counts;
-        return acc;
-      }, {});
-
-      try {
-        const totalValue = operation.evaluate(scope);
-        const response = transformObject([...result]);
-        return { value: response, total: totalValue };
-      } catch (error) {
-        return { searchId: calc.searchId, count: error.message };
-      }
+    newOperations.forEach((operation) => {
+      Object.entries(operation.variables).forEach(([key, hash]) => {
+        operation.variables[key] = hashToValue[hash];
+      });
+      operation.result = math.compile(operation.operation).evaluate(operation.variables);
     });
 
-    const results = await Promise.all(calculationPromises);
-    return new ServiceDTO(results, results.length, []);
+    return newOperations;
   }
 }
